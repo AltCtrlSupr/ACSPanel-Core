73c27f26c03368501889d24d914bb5c4927f5910
Genar Trias <g.trias@picmedia.es>
2015-01-13 17:10:16 +0100

    Cleaning code

--- Model/DomainRepository.php
+++ Model/DomainRepository.php
@@ -6,20 +6,12 @@
  */
 namespace ACS\ACSPanelBundle\Model;
 
-use Doctrine\ORM\EntityRepository;
 use ACS\ACSPanelUsersBundle\Entity\FosUser;
 use ACS\ACSPanelUsersBundle\Doctrine\AclEntityRepository;
 
 
 class DomainRepository extends AclEntityRepository
 {
-    private $acl_filter;
-
-    public function setAclFilter($acl_filter)
-    {
-        $this->acl_filter = $acl_filter;
-    }
-
     public function getUserViewable($user)
     {
 		$entities_raw = $this->_em->createQuery('SELECT d FROM ACS\ACSPanelBundle\Entity\Domain d');
