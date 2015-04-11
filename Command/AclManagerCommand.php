429b30958774ab6abbc322b01f0b2b114179f0b1
Genar Trias Ortiz <genar@acs.li>
2015-04-11 18:41:54 +0200

    Cleaning code and Creating aclcommand tests

--- Command/AclManagerCommand.php
+++ Command/AclManagerCommand.php
@@ -57,55 +57,55 @@ class AclManagerCommand extends ContainerAwareCommand
 
         $entities = $em->getRepository($name)->findAll();
 
-        foreach($entities as $entity){
+        foreach ($entities as $entity) {
 
-            foreach($this->domain_related_user_classes as $class){
-                if($entity instanceof $class){
+            foreach ($this->domain_related_user_classes as $class) {
+                if ($entity instanceof $class) {
                     $user = $entity->getDomain()->getUser();
-                    if($user)
+                    if ($user)
                         $output->writeln($this->addUserOwnerPermission($user, $entity));
                 }
             }
 
-            foreach($this->first_level_user_classes as $class){
-                if($entity instanceof $class){
+            foreach ($this->first_level_user_classes as $class) {
+                if ($entity instanceof $class) {
                     $user = $entity->getUser();
-                    if($user)
+                    if ($user)
                         $output->writeln($this->addUserOwnerPermission($user, $entity));
                 }
             }
 
-            if($entity instanceof ACS\ACSPanelUsersBundle\Entity\DnsRecord){
+            if ($entity instanceof ACS\ACSPanelUsersBundle\Entity\DnsRecord) {
                 $user = $entity->getDnsDomain()->getDomain()->getUser();
                 if($user)
                     $output->writeln($this->addUserOwnerPermission($user, $entity));
             }
 
-            if($entity instanceof ACS\ACSPanelUsersBundle\Entity\HttpdUser){
+            if ($entity instanceof ACS\ACSPanelUsersBundle\Entity\HttpdUser) {
                 $user = $entity->getHttpdHost()->getDomain()->getUser();
                 if($user)
                     $output->writeln($this->addUserOwnerPermission($user, $entity));
             }
 
-            if($entity instanceof ACS\ACSPanelUsersBundle\Entity\MailLogrcvd){
+            if ($entity instanceof ACS\ACSPanelUsersBundle\Entity\MailLogrcvd) {
                 $user = $entity->getMailDomain()->getUser();
-                if($user)
+                if ($user)
                     $output->writeln($this->addUserOwnerPermission($user, $entity));
             }
 
-            if($entity instanceof ACS\ACSPanelUsersBundle\Entity\MailMailBox){
+            if ($entity instanceof ACS\ACSPanelUsersBundle\Entity\MailMailbox) {
                 $user = $entity->getMailDomain()->getUser();
-                if($user)
+                if ($user)
                     $output->writeln($this->addUserOwnerPermission($user, $entity));
             }
 
-            if($entity instanceof ACS\ACSPanelUsersBundle\Entity\UserPlan){
+            if ($entity instanceof ACS\ACSPanelUsersBundle\Entity\UserPlan) {
                 $user = $entity->getPuser();
-                if($user)
+                if ($user)
                     $output->writeln($this->addUserOwnerPermission($user, $entity));
             }
 
-            foreach($superadmins as $superadmin){
+            foreach ($superadmins as $superadmin) {
                 $output->writeln($aclManager->addObjectPermission($entity, MaskBuilder::MASK_MASTER, $superadmin));
             }
 
@@ -116,7 +116,7 @@ class AclManagerCommand extends ContainerAwareCommand
 	{
 		$aclManager = $this->getContainer()->get('problematic.acl_manager');
 
-		if($parent = $user->getParentUser())
+		if ($parent = $user->getParentUser())
 			$aclManager->addObjectPermission($entity, MaskBuilder::MASK_MASTER, $parent);
 
 		$aclManager->addObjectPermission($entity, MaskBuilder::MASK_OWNER, $user);
@@ -124,6 +124,4 @@ class AclManagerCommand extends ContainerAwareCommand
         return "Added ". get_class($entity) . " Acls for ".$user;
 
 	}
-
-
 }
--- Controller/FosGroupController.php
+++ Controller/FosGroupController.php
@@ -45,11 +45,9 @@ class FosGroupController extends Controller
             throw $this->createNotFoundException('Unable to find FosGroup entity.');
         }
 
-        $deleteForm = $this->createDeleteForm($id);
-
         return $this->render('ACSACSPanelBundle:FosGroup:show.html.twig', array(
             'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),        ));
+        ));
     }
 
     /**
@@ -89,93 +87,5 @@ class FosGroupController extends Controller
             'entity' => $entity,
             'form'   => $form->createView(),
         ));
-    }
-
-    /**
-     * Displays a form to edit an existing FosGroup entity.
-     *
-     */
-    public function editAction($id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('ACSACSPanelUsersBundle:FosGroup')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find FosGroup entity.');
-        }
-
-        $editForm = $this->createForm(new FosGroupType(), $entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return $this->render('ACSACSPanelBundle:FosGroup:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Edits an existing FosGroup entity.
-     *
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('ACSACSPanelUsersBundle:FosGroup')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find FosGroup entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createForm(new FosGroupType(), $entity);
-        $editForm->bind($request);
-
-        if ($editForm->isValid()) {
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('groups_edit', array('id' => $id)));
-        }
-
-        return $this->render('ACSACSPanelBundle:FosGroup:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
-    }
-
-    /**
-     * Deletes a FosGroup entity.
-     *
-     */
-    public function deleteAction(Request $request, $id)
-    {
-        $form = $this->createDeleteForm($id);
-        $form->bind($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('ACSACSPanelUsersBundle:FosGroup')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find FosGroup entity.');
-            }
-
-            $em->remove($entity);
-            $em->flush();
-        }
-
-        return $this->redirect($this->generateUrl('groups'));
-    }
-
-    private function createDeleteForm($id)
-    {
-        return $this->createFormBuilder(array('id' => $id))
-            ->add('id', 'hidden')
-            ->getForm()
-        ;
     }
 }
