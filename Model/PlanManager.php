b16c0f7825798e2b15a6b1130b094fcddc8c7dac
Genar Trias Ortiz <genar@acs.li>
2016-01-08 19:53:59 +0100

    fixing tests

--- Model/PlanManager.php
+++ Model/PlanManager.php
@@ -8,4 +8,23 @@ namespace ACS\ACSPanelBundle\Model;
 
 class PlanManager
 {
+    private $em;
+
+    public function setEm($em)
+    {
+        $this->em = $em;
+    }
+
+    public function attachToUser($plan, $user)
+    {
+        $em = $this->em;
+
+        if ($user->addPlan($plan)) {
+            $em->persist($user);
+            $em->flush($user);
+            return true;
+        }
+
+        return false;
+    }
 }
